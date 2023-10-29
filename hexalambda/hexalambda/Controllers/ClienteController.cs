ea73bdaab87fd16a9a462f8ddfcb0800dec6f5d2
marayza.gonzaga <marayza.gonzaga@systax.com.br>
2023-10-29 14:10:25 -0300

    lambda para autenticacao de usuarios

---
+++ hexalambda/hexalambda/Controllers/ClienteController.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using hexalambda.Models;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace hexalambda.Controllers
+{
+    [Route("api/usuarios")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public ClienteController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("PostgreSQLConnection");
+        }
+
+        [HttpGet]
+        public IActionResult AutenticarPorCPF(string cpf)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+
+                try
+                {
+                    connection.Open();
+                }
+                catch(Exception e)
+                {
+                    return BadRequest("Erro banco de dados");
+                }
+
+                // Use Dapper para executar uma consulta SQL personalizada
+                var query = "SELECT * FROM clientes WHERE cpf = @CPF ";
+                var parameters = new { CPF = cpf};
+                var usuario = connection.QueryFirstOrDefault<Cliente>(query, parameters);
+
+                if (usuario == null)
+                    return NotFound("Usuário não encontrado");
+
+                return Ok(usuario);
+            }
+        }
+    }
+}
---
+++ hexalambda/hexalambda/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace hexalambda.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+}
